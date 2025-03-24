38ca286e09bd81ffd64aa5a51cacecd40ecc6335
fwhoami <clahfclns@gmail.com>
2025-03-24 22:58:59 +0200

    added-spring-boot-security

--- src/main/java/org/example/bookshop/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.bookshop.repository;
-
-import org.example.bookshop.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
