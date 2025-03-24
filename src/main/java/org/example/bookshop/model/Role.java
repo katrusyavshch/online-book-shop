a3941ad1ded7810aaa58aa7a931e87671ea07607
fwhoami <clahfclns@gmail.com>
2025-03-25 00:07:09 +0200

    Modify column definition: VARCHAR(255)

--- src/main/java/org/example/bookshop/model/Role.java
+++ src/main/java/org/example/bookshop/model/Role.java
@@ -8,18 +8,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "roles")
-@Data
+@Getter
+@Setter
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, columnDefinition = "VARCHAR")
     @Enumerated(EnumType.STRING)
     private RoleName name;
 
