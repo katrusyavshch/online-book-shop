886ad76e51e08020ebf76b8a9095c2ee9ffb7baf
fwhoami <clahfclns@gmail.com>
2025-03-30 20:47:27 +0300

    added jwt

--- src/main/java/org/example/bookshop/controller/AuthenticationController.java
+++ src/main/java/org/example/bookshop/controller/AuthenticationController.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.bookshop.dto.UserLoginRequestDto;
+import org.example.bookshop.dto.UserLoginResponseDto;
 import org.example.bookshop.dto.UserRegistrationRequestDto;
 import org.example.bookshop.dto.UserResponseDto;
 import org.example.bookshop.exception.RegistrationException;
+import org.example.bookshop.security.AuthenticationService;
 import org.example.bookshop.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Authentication", description = "API for user registration")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     @Operation(summary = "User registration",
@@ -26,5 +30,10 @@ public class AuthenticationController {
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
+        return authenticationService.authenticate(request);
     }
 }
---
+++ src/main/java/org/example/bookshop/dto/UserLoginResponseDto.java
@@ -0,0 +1,4 @@
+package org.example.bookshop.dto;
+
+public record UserLoginResponseDto(String token) {
+}
