4a63a9f2856a7b1fde8d3737da7d8afccc734d84
fwhoami <clahfclns@gmail.com>
2025-03-31 23:41:27 +0300

    add some fixing to UserServiceImpl

--- src/main/java/org/example/bookshop/service/UserServiceImpl.java
+++ src/main/java/org/example/bookshop/service/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package org.example.bookshop.service;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.example.bookshop.dto.UserRegistrationRequestDto;
 import org.example.bookshop.dto.UserResponseDto;
 import org.example.bookshop.exception.RegistrationException;
+import org.example.bookshop.model.RoleName;
 import org.example.bookshop.mapper.UserMapper;
 import org.example.bookshop.model.User;
+import org.example.bookshop.repository.role.RoleRepository;
 import org.example.bookshop.repository.user.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -22,6 +28,8 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException("Email address already in use");
         }
         User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        user.setRoles(Set.of(roleRepository.findByName(RoleName.USER)));
         userRepository.save(user);
         return userMapper.toResponseDto(user);
 
