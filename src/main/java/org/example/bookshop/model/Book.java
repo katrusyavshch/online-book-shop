5d4a48fcebc7c5e6587c3742e15482e149bae093
fwhoami <clahfclns@gmail.com>
2025-04-07 17:57:42 +0300

    verify

--- src/main/java/org/example/bookshop/model/Book.java
+++ src/main/java/org/example/bookshop/model/Book.java
@@ -12,10 +12,8 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
